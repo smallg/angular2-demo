569c1767f90f7d6ba035931e6b47cc59d6f2b80f
gwpxjb@163.com <gwpxjb@163.com>
2017-11-01 09:37:52 +0800

    work on official demo.

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroService} from "./hero.service";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import {HeroDetailComponent} from "./hero-detail.component";
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
