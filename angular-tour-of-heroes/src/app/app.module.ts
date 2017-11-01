51780c07d4927c4248e32d5633b908cdc1acf9bf
gwpxjb@163.com <gwpxjb@163.com>
2017-11-01 09:25:37 +0800

    work on official demo.

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "./hero-detail.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
