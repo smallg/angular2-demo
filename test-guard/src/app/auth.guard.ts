500be3545ca1e558e92ac94798b433b2e56225f1
gwpxjb@163.com <gwpxjb@163.com>
2018-03-09 21:59:41 +0800

    ng serve --env=prod or ng serve to switch prod

--- test-guard/src/app/auth.guard.ts
+++ test-guard/src/app/auth.guard.ts
@@ -1,14 +1,18 @@
 import {Injectable} from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
+import {environment} from "../environments/environment";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(next: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    console.log(environment.apiUrl);
     if (localStorage.getItem('user')) {
+      console.log('access');
       return true;
     } else {
+      console.error('invalid auth');
       return false;
     }
   }
